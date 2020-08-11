18a7477ea64f81dba51cced401eb1c582959aab9
ozzmotic <ozzmotic@gmail.com>
2020-08-11 10:54:17 -0400

    Embed Google Calendar, add styling to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,44 @@ import Layout from 'components/layout';
 import Box from 'components/box';
 import Title from 'components/title';
 import { graphql } from 'gatsby';
+import styled from 'styled-components';
+
+{/* Styling for a two-column flex layout for this homepage */}
+const HomeWrapper = styled.div`
+  margin: 3rem auto;
+  max-width: 600px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`
+
+const Description = styled.h1`
+  flex: 1;
+  margin-left: 18px;
+  padding: 12px;
+`
+const calendarStyle = {
+  border: 'solid 10px #4cc0ad',
+  order: '2', /* Flex order */
+}
 
 const Index = ({ data }) => (
   <Layout>
     <Box>
-      <Title as="h2" size="large">
+      <Title as="h1" size="large">
         {data.homeJson.content.childMarkdownRemark.rawMarkdownBody}
       </Title>
     </Box>
+    <HomeWrapper style={{height:'500px'}}>
+      <p>Congress visualization placeholder</p>
+    </HomeWrapper>
+    <HomeWrapper>
+      <Description>Join us for our upcoming events.</Description>
+
+      {/* Embed Google Calendar */}
+      <iframe title="eewCalendar" src="https://calendar.google.com/calendar/embed?height=300&amp;wkst=2&amp;bgcolor=%234cc0ad&amp;ctz=America%2FNew_York&amp;src=c280bXJsNjk5YWVhNTE5bnQxNzhwNTBwMzhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23E4C441&amp;showTitle=0&amp;showNav=0&amp;showDate=1&amp;showPrint=0&amp;showTabs=1&amp;showCalendars=0&amp;mode=AGENDA" style={calendarStyle} width="400" height="300" frameBorder="0" scrolling="no"></iframe>
+    </HomeWrapper>
     <div style={{ height: '50vh' }} />
   </Layout>
 );
