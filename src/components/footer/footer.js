e708d2f17b2adc5e2032b3d1f7946f07e87cc963
Kelsey Breseman <ifoundthemeaningoflife@gmail.com>
2020-08-19 15:57:34 -0700

    adds template divs in the footer for future content

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,11 +4,25 @@ import { Container } from './footer.css';
 
 const Footer = () => (
   <Container>
-    <div>
+    <div id="get-involved">
+      
+    </div>
+    <div id="call-to-action">
       <p>Together we can hold industry and environmental regulators accountable. Join us.</p>
+    </div>
+    <div id="contribute">
+    
+    </div>
+    <div id="hashtags">
       <p>#EEWatch</p>
       <p>#EnvironmentalDataJustice</p>
       <p>#YouCanEEWToo</p>
+    </div>
+    <div id="EDGI-logo">
+    
+    </div>
+    <div id="socials">
+    
     </div>
   </Container>
 );
