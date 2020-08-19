1239e4b8dfc264073fa51a0e5af349a1867b167d
Kelsey Breseman <ifoundthemeaningoflife@gmail.com>
2020-08-19 16:20:57 -0700

    adds a basic press page towards https://github.com/edgi-govdata-archiving/EEW-Website/issues/5

---
+++ content/press/press.md
@@ -0,0 +1,3 @@
+<!--This is the main content file to edit for this page. It is embedded in src/pages/about.js-->
+
+[Contact](info@environmentalenforcementwatch.org) a member of EDGI's EEW project team.
---
+++ src/pages/press.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
+import Layout from 'components/layout';
+import Box from 'components/box';
+import Head from 'components/head';
+
+const Press = ({ data }) => (
+  <Layout>
+    <Head pageTitle={data.pressJson.title} />
+    <Box>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: data.pressJson.content.childMarkdownRemark.html,
+        }}
+      />
+    </Box>
+  </Layout>
+);
+
+Press.propTypes = {
+  data: PropTypes.object.isRequired,
+};
+
+export default Press;
+
+export const query = graphql`
+  query PressQuery {
+    pressJson {
+      title
+      content {
+        childMarkdownRemark {
+          html
+        }
+      }
+    }
+  }
+`;
