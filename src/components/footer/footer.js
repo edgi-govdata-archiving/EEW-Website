af7e5cbd8763d4bfd9f765cee5d500d834bfe079
Kelsey Breseman <ifoundthemeaningoflife@gmail.com>
2020-08-19 15:48:55 -0700

    there's a footer now! relevant to https://github.com/edgi-govdata-archiving/EEW-Website/issues/15

---
+++ src/components/footer/footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import GlobalStyle from 'global.css.js';
+import { Container } from './footer.css';
+
+const Footer = () => (
+  <Container>
+    <div>
+      <p>Together we can hold industry and environmental regulators accountable. Join us.</p>
+      <p>#EEWatch</p>
+      <p>#EnvironmentalDataJustice</p>
+      <p>#YouCanEEWToo</p>
+    </div>
+  </Container>
+);
+
+export default Footer;
