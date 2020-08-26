cc7b875c89467d8caf6d5e34282c16e4a307bc04
ozzmotic <ozzmotic@gmail.com>
2020-08-26 14:07:53 -0400

    Re-adds React hook

--- src/components/circle/circle.js
+++ src/components/circle/circle.js
@@ -1,45 +1,44 @@
-import React, { useEffect, useRef } from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect, useRef } from "react";
 import { select } from 'd3';
-import useResizeObserver from 'helpers/useResizeObserver';
 import styles from './circle.module.css';
 import { useCongressData } from 'hooks/use-congress-data';
 
 function Circle() {
-  //const svgRef = useRef();
+  const figureRef = useRef();
   const congressData = useCongressData();
 
   // Select the figure as declared in the layout section (at bottom)
   // d3 manipulation of div elements rather than svg/rect elements from: https://pudding.cool/process/flexbox-layout/
-  const fig = select('.viz');
 
-  fig
-    .attr("class", styles.viz );
+  useEffect(() => {
+    const fig = select(figureRef.current);
 
-  fig
-    .selectAll(".rep")
-    .data(congressData)
-    .join('div')
-    .each(function (d) {
-      var thisRep = select(this);
+    fig
+      .selectAll('div')
+      .data(congressData) //Attach data
+      .join('div') //Create divs
+      .each(function (d) {
+        //Apply stylings to each representative
+        var thisRep = select(this);
 
-      if (d.affil == "gop") {
-        thisRep.style("background-color","red");
-      } else {
-        thisRep.style("background-color","blue");
-      }
+        if (d.affil == "gop") {
+          thisRep.style("background-color","red");
+        } else {
+          thisRep.style("background-color","blue"); 
+        }
 
-      if (d.rank == "chair") {
-        thisRep.attr("class", styles.chair);
-      } else {
-        thisRep.attr("class", styles.rep);
-      }
+        if (d.rank == "chair") {
+          thisRep.attr("class", styles.chair);
+        } else {
+          thisRep.attr("class", styles.rep);
+        }
 
-      thisRep.text(d.state) 
-    });
-  
+        thisRep.text(d.state)
+      });
+  }, [congressData]);
+
   return (
-    <figure className="viz" >
+    <figure ref={figureRef} className={styles.viz}>  
     </figure>
   )
 }
