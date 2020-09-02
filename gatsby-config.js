a0a2735f97dd830ae22c75f797c48b8dd951fa54
ozzmotic <ozzmotic@gmail.com>
2020-09-02 08:39:13 -0400

    Add gatsby-remark-images plugin to use default Markdown syntax for images

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,12 @@ module.exports = {
                 }
               ]
             }
+          },
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 800,
+            }
           }          
         ]
       }
