2bf86e3621a3a4446014885b5541df04e80cf3a1
ozzmotic <ozzmotic@gmail.com>
2020-09-03 12:13:31 -0400

    Style footer by adding contribute heading and vertically stacking elements

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -10,18 +10,19 @@ const Footer = () => (
       <EmailListForm />
     </div>
     <div id="contribute">
+      <h2>Contribute to EDGI</h2>
       <div data-ab-button
         data-ab-token="MtYXR6eU4AKeV3oqqj3pdmuK"
         data-ab-amounts="5,10,25,50,100,250,n"
-        data-ab-style="background-color: #4fbba9;"
+        data-ab-style="background-color: #d43A69;"
       >
       <a href="https://secure.actblue.com/donate/edgi_eew">Donate</a>
     </div>
     </div>
     <div id="hashtags">
-      <p>#EEWatch</p>
-      <p>#EnvironmentalDataJustice</p>
-      <p>#YouCanEEWToo</p>
+      <h2>#EEWatch</h2>
+      <h2>#EnvironmentalDataJustice</h2>
+      <h2>#YouCanEEWToo</h2>
     </div>
     <div id="EDGI-logo">
     
