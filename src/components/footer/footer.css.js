8ff07ca649f015dc7579347d021fada275cce803
ozzmotic <ozzmotic@gmail.com>
2020-09-16 16:49:06 -0400

    Style footer

--- src/components/footer/footer.css.js
+++ src/components/footer/footer.css.js
@@ -7,9 +7,11 @@ export const Container = styled.footer`
   padding: 4rem;
   background-color: #E3F9F3;
   flex-flow: column;
+  text-align:center;
 
   #contribute {
     padding: 2rem;
+    margin: 4rem;
     width: 400px;
     flex-shrink: 1;
   }
