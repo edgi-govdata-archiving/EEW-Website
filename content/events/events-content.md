4b13269fabab4b6757f3b934de87fa8ac50b3c01
Kelsey Breseman <ifoundthemeaningoflife@gmail.com>
2020-09-28 15:31:07 -0700

    adds events page

---
+++ content/events/events-content.md
@@ -0,0 +1,1 @@
+<!--This is the main content file to edit for this page. It is embedded in src/pages/events.js. The page title comes from the .json in this same folder.-->
--- src/pages/events.js
+++ src/pages/events.js
@@ -6,29 +6,29 @@ import Box from 'components/box';
 import Head from 'components/head';
 import styled from 'styled-components';
 
-const About = ({ data }) => (
+const Events = ({ data }) => (
   <Layout>
-    <Head pageTitle={data.aboutJson.title} />
+    <Head pageTitle={data.eventsJson.title} />
     <Box>
-      <h1>{data.aboutJson.title}</h1>
+      <h1>{data.eventsJson.title}</h1>
       <div
         dangerouslySetInnerHTML={{
-          __html: data.aboutJson.content.childMarkdownRemark.html,
+          __html: data.eventsJson.content.childMarkdownRemark.html,
         }}
       />
     </Box>
   </Layout>
 );
 
-About.propTypes = {
+Events.propTypes = {
   data: PropTypes.object.isRequired,
 };
 
-export default About;
+export default Events;
 
 export const query = graphql`
-  query AboutQuery {
-    aboutJson {
+  query EventsQuery {
+    eventsJson {
       title
       content {
         childMarkdownRemark {
