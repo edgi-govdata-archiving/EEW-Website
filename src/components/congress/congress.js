75cfece44ec6654adceec62e6169b0b64e0a6443
Kelsey Breseman <ifoundthemeaningoflife@gmail.com>
2020-10-07 12:28:35 -0700

    bolds status and removes word committee per https://github.com/edgi-govdata-archiving/EEW-Website/issues/132

--- src/components/congress/congress.js
+++ src/components/congress/congress.js
@@ -78,21 +78,27 @@ function Congress({ chamber }) {
       .on('mouseover', function(d) {
         select(this).classed(styles.repOnHover, true);
 
-        //Set the text of the tooltip
-        var tooltipText =
-          d.name +
-          '\nCommittee ' +
-          d.rank +
+        // This section sets up contents of tooltip
+        
+        // Include district if applicable
+        var infoText =
           '\n' +
           d.affil +
           '\nRepresenting ' +
           d.state;
         if (d.district) {
           // d.district undefined for senate because not called in graphQL query
-          tooltip.text(tooltipText + ' ' + d.district);
-        } else {
-          tooltip.text(tooltipText);
+          infoText = infoText + ' ' + d.district;
         }
+        
+        // Set the text of the tooltip
+        tooltip.text(d.name)
+          .append("tspan")
+          .style("font-weight",700)
+          .text('\n' + d.rank)
+          .append("tspan")
+          .style("font-weight",300)
+          .text(infoText)
 
         // Position tooltips at mouse location
         var x = event.x,
