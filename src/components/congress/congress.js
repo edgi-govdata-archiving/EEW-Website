e385e2d098f638c9367e3dfdd57ebec4824b9b52
Kelsey Breseman <ifoundthemeaningoflife@gmail.com>
2020-10-12 12:46:30 -0700

    there are only three rank levels per https://github.com/edgi-govdata-archiving/EEW-Website/issues/199

--- src/components/congress/congress.js
+++ src/components/congress/congress.js
@@ -60,9 +60,9 @@ function Congress({ chamber }) {
         }
 
         // Apply a different class if the rep is a chair (in thise case, to make it larger than the regular reps)
-        if (d.rank == 'Chair' || d.rank == 'Ranking Member' || d.rank == 'Vice Chair') {
+        if (d.rank == 'Chair') {
           thisRep.classed(styles.chair, true);
-        } else if (d.rank == 'Ranking Member' || d.rank == 'Vice Chair') {
+        } else if (d.rank == 'Ranking Member') {
           thisRep.classed(styles.ranking, true);
         } else {
           thisRep.classed(styles.rep, true);
