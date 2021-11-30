e4ca7a217f234b3dac43bf6bbbc46a65551fc6a7
CaptainStack <captainstack@gmail.com>
2021-11-30 14:13:15 -0800

    Create new components location, move/create components

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import * as React from "react"
+import EDGILogo from "../images/EDGI-logo.png"
+
+const Footer = () =>
+  <footer>
+    <div className='container'>
+      <div>
+        <a href='https://twitter.com/EEW_Network'>Twitter</a>
+        <a href='https://www.facebook.com/envirodgi'>Facebook</a>
+        <a href='https://github.com/edgi-govdata-archiving/Environmental-Enforcement-Watch'>Github</a>
+        <a href='mailto:info@environmentalenforcementwatch.org'>Email</a>
+      </div>
+      <a href='https://envirodatagov.org/' target='_blank' rel='noopener noreferrer'><img src={EDGILogo} /></a>
+      <p>© Environmental Data Governance Initiative (EDGI) {new Date().getFullYear()}. | EDGI is a project of Multiplier, a tax-exempt nonprofit 501(c)3 umbrella organization (Tax ID 91-2166435).</p>
+    </div>
+  </footer>
+
+export default Footer
---
+++ src/components/Layout.js
@@ -0,0 +1,25 @@
+import * as React from "react"
+import Header from '../components/Header'
+import Head from '../components/Head'
+// import Notification from '../components/Notification'
+import Sidebar from '../components/Sidebar'
+import Footer from '../components/Footer'
+import '../styles/styles.scss'
+
+const Layout = ({pageTitle, children, sidebarLinks, activeHeaderLink}) => 
+  <div className='Layout'>
+    <Head pageTitle={pageTitle} />
+    <Header activeLink={activeHeaderLink}/>
+    {/* <Notification /> */}
+    <div className='middle'>
+      <div className='container'>
+        {sidebarLinks ? <Sidebar links={sidebarLinks} pageTitle={pageTitle}/> : null}
+        <main>
+          {children}
+        </main>
+      </div>
+    </div>
+    <Footer />
+  </div>
+
+export default Layout
