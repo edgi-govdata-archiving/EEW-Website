ef558e9a5824e1a07182a88f970429863f103182
CaptainStack <captainstack@gmail.com>
2021-12-03 12:07:13 -0800

    Remove deprecated & boilierplate comments

--- src/pages/about/contact.js
+++ src/pages/about/contact.js
@@ -1,11 +1,9 @@
-// Step 1: Import React
 import * as React from 'react'
 import Layout from '../../components/Layout'
 import { AboutSidebarLinks } from '../../helpers/constants'
 
 const ContactPage = () => 
   <Layout pageTitle="Contact" sidebarLinks={AboutSidebarLinks} activeHeaderLink="About">
-    {/* <p><strong>Press Contact: <a href='environmentalenforcementwatch@gmail.com'>environmentalenforcementwatch@gmail.com</a></strong></p> */}
     <h1>Contact</h1>
       <form action="https://submit-form.com/y28zl2Mj">
       <fieldset>
@@ -30,5 +28,4 @@ const ContactPage = () =>
       </form>
   </Layout>
 
-// Step 3: Export your component
 export default ContactPage
