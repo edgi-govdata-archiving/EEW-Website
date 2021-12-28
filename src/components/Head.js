0835edce199348c4524ea2d62e2fd873a8967e2d
Kelsey Breseman <ifoundthemeaningoflife@gmail.com>
2021-12-28 13:07:22 -0800

    typo fix in head

--- src/components/Head.js
+++ src/components/Head.js
@@ -10,7 +10,7 @@ const Head = ({pageTitle}) =>
     <html lang="en" />
     <title>{pageTitle} | Environmental Enforcement Watch</title>
     <meta content='https://environmentalenforcementwatch.org/static/1c753d31dbdd94ab2b736e4da6ae656a/0a47e/eew-map.png' property="og:image" />
-    <meta property="og:description" content="A data accessibilty project from the Environmental Data Governance Initiative" />
+    <meta property="og:description" content="A data accessibility project from the Environmental Data & Governance Initiative" />
 
     {/* Global site tag (gtag.js) - Google Analytics  */}
     <script
