793a03c3b245c13b24d7551da08f622800105360
Andre Stackhouse <captainstack@gmail.com>
2022-01-10 21:04:53 -0800

    Add hCaptcha to contact form

--- src/pages/about/contact.js
+++ src/pages/about/contact.js
@@ -1,30 +1,33 @@
 import * as React from 'react'
 import Layout from '../../components/Layout'
 import { AboutSidebarLinks } from '../../helpers/constants'
+import HCaptcha from '@hcaptcha/react-hcaptcha';
 
 {/* This contact page is implemented using FormSpark - to make changes to how it works, log into EDGI's FormSpark account and configure using the online dashboard */}
 const ContactPage = () => 
   <Layout pageTitle="Contact" sidebarLinks={AboutSidebarLinks} activeHeaderLink="About">
     <h1>Contact</h1>
       <form action="https://submit-form.com/y28zl2Mj">
-      <fieldset>
-        <div>
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name" required="" />
-        </div>
-        <div>
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" required="" />
-        </div>
-        <div>
-        <label htmlFor="message">Message</label>
-        <textarea
-          id="message"
-          name="message"
-          required="">
-        </textarea>
-        <button className='button' type="submit">Send</button>
-        </div>
+        
+        <fieldset>
+          <div>
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" name="name" required="" />
+          </div>
+          <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" required="" />
+          </div>
+          <div>
+          <label htmlFor="message">Message</label>
+          <textarea
+            id="message"
+            name="message"
+            required="">
+          </textarea>
+          <button className='button' type="submit">Send</button>
+          <HCaptcha sitekey="10556620-2ec1-454e-b351-23a806325156" />
+          </div>
         </fieldset>
       </form>
   </Layout>
