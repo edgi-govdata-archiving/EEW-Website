791604380418dcba404cf19baa2ea856a4d7164c
Andre Stackhouse <captainstack@gmail.com>
2022-01-10 23:29:51 -0800

    Remove GA script, remove references from footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,18 @@
 import * as React from "react"
 import EDGILogo from "../images/EDGI-logo.png"
-import { googleAnalyticsOutboundLinksTracker } from "../helpers/google-analytics-outbound-links-tracker"
 
-const Footer = () => {
-  googleAnalyticsOutboundLinksTracker;
-  return (
-    <footer>
-      <div className='container'>
-        <div>
-          <a href='https://twitter.com/EEW_Network'>Twitter</a>
-          <a href='https://www.facebook.com/envirodgi'>Facebook</a>
-          <a href='https://github.com/edgi-govdata-archiving/Environmental-Enforcement-Watch'>Github</a>
-          <a href='mailto:info@environmentalenforcementwatch.org'>Email</a>
-        </div>
-        <a href='https://envirodatagov.org/' target='_blank' rel='noopener noreferrer'><img src={EDGILogo} /></a>
-        <p>© Environmental Data Governance Initiative (EDGI) {new Date().getFullYear()}. | EDGI is a project of Multiplier, a tax-exempt nonprofit 501(c)3 umbrella organization (Tax ID 91-2166435).</p>
+const Footer = () => 
+  <footer>
+    <div className='container'>
+      <div>
+        <a href='https://twitter.com/EEW_Network'>Twitter</a>
+        <a href='https://www.facebook.com/envirodgi'>Facebook</a>
+        <a href='https://github.com/edgi-govdata-archiving/Environmental-Enforcement-Watch'>Github</a>
+        <a href='mailto:info@environmentalenforcementwatch.org'>Email</a>
       </div>
-    </footer>
-  )
-}
+      <a href='https://envirodatagov.org/' target='_blank' rel='noopener noreferrer'><img src={EDGILogo} /></a>
+      <p>© Environmental Data Governance Initiative (EDGI) {new Date().getFullYear()}. | EDGI is a project of Multiplier, a tax-exempt nonprofit 501(c)3 umbrella organization (Tax ID 91-2166435).</p>
+    </div>
+  </footer>
 
 export default Footer
